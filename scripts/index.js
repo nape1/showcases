5c5b4ccac81c7fdee27512552a26a20cff3be8c5
nape1 <najinshah.pearson@gmail.com>
2024-01-11 00:36:49 +0530

    scripts added

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    console.log('window onload');
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(window.location.href,'DOMContentLoaded');
+});
